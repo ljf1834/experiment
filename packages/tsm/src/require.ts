d36d80acf0db6b3a315bc1996daf11a14b7560d3
liujinfu <1834262609@qq.com>
2022-12-03 19:10:58 +0800

    chore: CommonJs resolve

--- packages/tsm/src/require.ts
+++ packages/tsm/src/require.ts
@@ -1,3 +1,31 @@
-const loadJs = require.extensions['.js'];
+import { format } from 'path';
 
-console.log('loadJs', loadJs);
+const esbuild = require('esbuild') as typeof import('esbuild');
+const { extname } = require('path') as typeof import('path');
+const { readFileSync } = require('fs') as typeof import('fs');
+
+const loadJS = require.extensions['.js']
+const transform = (source: string, options = {}): string => esbuild.transformSync(source, options).code
+
+function loader(Module, sourcefile: string) {
+  let extn = extname(sourcefile)
+  let pitch = Module._compile!.bind(Module);
+  if (sourcefile.endsWith('.ts')) {
+    Module._compile = source => {
+      let result = transform(source, { format: 'cjs' });
+      return pitch(result, sourcefile);
+    }
+  }
+  try {
+    return loadJS(Module, sourcefile);
+  } catch (err) {
+    let ec = err && (err as any).code;
+    if (ec !== 'ERR_REQUIRE_ESM') throw err;
+
+    let input = readFileSync(sourcefile, 'utf8');
+    let result = transform(input, { format: 'cjs' });
+    return pitch(result, sourcefile);
+  }
+}
+
+require.extensions['.ts'] = loader
